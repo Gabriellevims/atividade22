bfd2f77ae05a883780bcbb78915ef44fabe22740
gabriellevims <gabriellevi2104@gmail.com>
2023-10-31 18:26:50 -0300

    números pa de 1 a 50

---
+++ atividade22.py
@@ -0,0 +1,4 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 
+#(dica estude range e seus paramentros)
+print("números pares de 1 a 50:")
+print(list(range(2,51,2)))
